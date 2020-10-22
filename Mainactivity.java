fd12326b923f451fc33d1a4cb5df1dc9a2ba37bf
iamroot007 <64304479+iamroot007@users.noreply.github.com>
2020-10-22 13:06:27 +0530

    Fixed SOme Bugs

--- Mainactivity.java
+++ Mainactivity.java
@@ -10,52 +10,38 @@ import com.nitin.app1.EvaluateString;
 public class MainActivity extends AppCompatActivity {
     Button  buttonC;
     EditText Answer;
-    String Exp,TempText;
+    String Exp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Answer=(EditText)findViewById(R.id.edt1);
         Exp="";
-        TempText="";
     }
     public void handleinput(View view) {
              Button b = (Button)view;
-             TempText=TempText+b.getText()+"";
-             Answer.setText(TempText);
+             Answer.setText(Answer.getText()+""+b.getText()+"");
     }
     public void handleoperand(View view){
             Button b = (Button)view;
-            if (TempText==""||Answer.getText().toString()==""){
+            if (Answer.getText().toString()==""){
             if (Exp.endsWith("+")||Exp.endsWith("-")||Exp.endsWith("/")||Exp.endsWith("*")){
               Exp = Exp.substring(0, Exp.length() - 1) + b.getText()+"";
             }}
             else{
-             if (Answer.getText()+""==TempText) {
-               Exp = Exp + TempText+b.getText()+"";
-              }
-             else {
                 Exp=Exp+Answer.getText()+b.getText()+"";
-            }}
-            TempText="";
-            Answer.setText("0");
+            }
+            Answer.setText("");
     }
-        public void handlesubmit(View view){
-         if (TempText==null) {
-                Exp = Exp + TempText;
-            }
-         else
-            {
-                Exp=Exp+Answer.getText()+"";
-            }
-         double ans=EvaluateString.eval(Exp);
-         Answer.setText(ans+"");
-         Exp=ans+"";
-        }
-        public void clearall(View view){
+    public void handlesubmit(View view){
+        Exp = Exp + Answer.getText()+"";
+        double ans=EvaluateString.eval(Exp);
+        Answer.setText(ans+"");
+        Exp=ans+"";
+    }
+    public void clearall(View view){
           Exp="";
-          TempText="";
-          Answer.setText("0");
-        }
+          Answer.setText("");
+    }
 
 }
