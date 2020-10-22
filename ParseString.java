800a015ee9c00b39b748f71a40b0f24d2c42de3c
iamroot007 <64304479+iamroot007@users.noreply.github.com>
2020-10-22 13:08:50 +0530

    Removed Extra data

--- ParseString.java
+++ ParseString.java
@@ -45,7 +45,6 @@ public class EvaluateString {
             double parseFactor() {
                 if (eat('+')) return parseFactor();
                 if (eat('-')) return -parseFactor();
-
                 double x;
                 int startPos = this.pos;
                 if (eat('(')) {
@@ -54,20 +53,11 @@ public class EvaluateString {
                 } else if ((ch >= '0' && ch <= '9') || ch == '.') {
                     while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
                     x = Double.parseDouble(str.substring(startPos, this.pos));
-                } else if (ch >= 'a' && ch <= 'z') {
-                    while (ch >= 'a' && ch <= 'z') nextChar();
-                    String func = str.substring(startPos, this.pos);
-                    x = parseFactor();
-                    if (func.equals("sqrt")) x = Math.sqrt(x);
-                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-                    else throw new RuntimeException("Unknown function: " + func);
-                } else {
+                }
+                 else {
                     throw new RuntimeException("Unexpected: " + (char)ch);
                 }
 
-                if (eat('^')) x = Math.pow(x, parseFactor());
 
                 return x;
             }
